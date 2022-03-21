5acbf15dd81ea9de73df862b482b5f2e0720587a
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-03-21 13:51:02 +0100

    add log-log fitting routine and write exponent to results

--- source/zipf_test.py
+++ source/zipf_test.py
@@ -1,6 +1,6 @@
 from wordcount import load_word_counts
+import numpy as np
 import sys
-
 
 def top_n_words(counts, number_of_top_words=10):
     """
@@ -11,6 +11,13 @@ def top_n_words(counts, number_of_top_words=10):
     count_data = [count for (_, count, _) in limited_counts]
     return count_data
 
+def fit_linear_loglog(row):
+    X = np.log(np.arange(len(row)) + 1.0)
+    ones = np.ones(len(row))
+    A = np.vstack((X, ones)).T
+    Y = np.log(row)
+    res = np.linalg.lstsq(A, Y, rcond=-1)
+    return res[0][0]
 
 if __name__ == '__main__':
     number_of_top_words = sys.argv[1]
@@ -18,12 +25,16 @@ if __name__ == '__main__':
     header = "Book"
     for i in range(int(number_of_top_words)):
          header += ",word"+str(i+1)
+
+    header += ",power law exponent"
     print(header)
     for input_file in input_files:
         counts = load_word_counts(input_file)
         top_words = top_n_words(counts, int(number_of_top_words))
+        exponent = fit_linear_loglog(top_words)
         bookname = input_file[:-4].split("/")[-1]
         line = bookname
         for i in top_words:
             line += ","+str(i)
+        line += ","+str(exponent)
         print(line)
