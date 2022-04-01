017d79144eae62bb27e0b33e479f781d1493ba6f
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-04-01 11:21:10 +0200

    final version with both p2p and collective implementations

--- source/autocorrelation.py
+++ source/autocorrelation.py
@@ -45,6 +45,49 @@ def word_autocorr_average(words, text, timesteps=100):
         acf[n, :] = word_autocorr(word, text, timesteps)
     return np.average(acf, axis=0)
 
+def word_count_average_mpi_collective(my_words, text, rank, n_ranks, timesteps=100):
+    # each rank computes its own set of acfs
+    my_acfs = np.zeros((len(my_words), timesteps))
+    for i, word in enumerate(my_words):
+        my_acfs[i,:] = word_autocorr(word, text, timesteps)
+
+    # gather results on rank 0
+    results = comm.gather(my_acfs, root=0)
+    # loop over ranks and results. result is a list of lists of ACFs
+    if rank == 0:
+        acf_tot = np.zeros((timesteps,))
+        for i in range(n_ranks):
+            for j in range(len(results[i])):
+                acf_tot += results[i][j]
+        # compute average and write to file
+        acf_ave = acf_tot / nwords
+        return acf_ave
+
+def word_count_average_mpi_p2p(my_words, text, rank, n_ranks, timesteps=100):
+    # each rank computes its own set of acfs
+    my_acfs = np.zeros((len(my_words), timesteps))
+    for i, word in enumerate(my_words):
+        my_acfs[i,:] = word_autocorr(word, text, timesteps)
+
+    if rank == 0:
+        results = []
+        # append own results
+        results.append(my_acfs)
+        # receive data from other ranks and append to results
+        for sender in range(1, n_ranks):
+            results.append(comm.recv(source=sender, tag=12))
+        # compute average and write to file
+        acf_tot = np.zeros((timesteps,))
+        for i in range(n_ranks):
+            for j in range(len(results[i])):
+                acf_tot += results[i][j]
+        acf_ave = acf_tot / nwords
+        return acf_ave
+    else:
+        # send data
+        comm.send(my_acfs, dest=0, tag=12)
+
+
 if __name__ == '__main__':
     # load book text and preprocess it
     book = sys.argv[1]
@@ -55,7 +98,9 @@ if __name__ == '__main__':
     nwords = 10
     word_count = load_word_counts(wc_book)
     top_words = [w[0] for w in word_count[:nwords]]
-    
+    # number of "timesteps" to use in autocorrelation function
+    timesteps = 100
+
     # initialize MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -76,37 +121,14 @@ if __name__ == '__main__':
     my_words = top_words[first:last]
     print(f"My rank number is {rank} and first, last = {first}, {last}")
 
-    # number of "timesteps" to use in autocorrelation function
-    timesteps = 100
+    # use collective function
+    #acf_ave = word_count_average_mpi_collective(my_words, clean_text, rank, n_ranks, timesteps=100)
 
-    # only rank 0 will collect all acfs
-    if rank == 0:
-        acf_all = np.zeros((nwords, timesteps))
-    # each rank computes own set of acfs
-    my_acfs = np.zeros((len(my_words), timesteps))
-    for i, word in enumerate(my_words):
-        my_acfs[i,:] = word_autocorr(word, clean_text, timesteps)
+    # use p2p function
+    acf_ave = word_count_average_mpi_p2p(my_words, clean_text, rank, n_ranks, timesteps=100)
 
-    # rank 0 receives data from other ranks
+    # only rank 0 has the averaged data
     if rank == 0:
-        # first copy own my_acfs to acf_all
-        for n, i in enumerate(range(first, last)):
-            acf_all[i,:] = my_acfs[n,:]
-        # then receive from other workers
-        for sender in range(1, n_ranks):
-            # first receive indices
-            rec_first, rec_last = comm.recv(source=sender, tag=10)
-            # then receive data
-            acf_all[rec_first:rec_last,:] = comm.recv(source=sender, tag=12)
-    else:
-        # first send indices
-        comm.send([first, last], dest=0, tag=10)
-        # then send data
-        comm.send(my_acfs, dest=0, tag=12)
-
-    # rank 0 computes average and saves result
-    if rank == 0:
-        acf_ave = np.average(acf_all, axis=0)
         np.savetxt(sys.argv[3], np.vstack((np.arange(1,101), acf_ave)).T, delimiter=',')
 
 
